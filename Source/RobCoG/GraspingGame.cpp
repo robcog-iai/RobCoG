c7c82b3c2491e3c9a323cad1fb68ce0d6244e9e8
Marcel Meier <meierma@uni-bremen.de>
2017-08-07 17:14:01 +0200

    Edit GraspGame

--- Source/RobCoG/GraspingGame.cpp
+++ Source/RobCoG/GraspingGame.cpp
@@ -1,6 +1,8 @@
 // Copyright 2017, Institute for Artificial Intelligence - University of Bremen
 
 #include "GraspingGame.h"
+#include "Paths.h"
+#include "FileManagerGeneric.h"
 
 
 // Sets default values
@@ -9,6 +11,7 @@ AGraspingGame::AGraspingGame()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Paths.Add("Items/Scanned/Meshes");
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +19,15 @@ void AGraspingGame::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	FString AbsolutePath = "";
+	auto ContentFolder = FPaths::GameContentDir();
+
+	for (auto && Path : Paths)
+	{
+		AbsolutePath = ContentFolder + Path;
+		UE_LOG(LogTemp, Warning, TEXT("AbsolutePath: %s"), *AbsolutePath);
+		GetAssetsInFolder(AbsolutePath, Items);
+	}	
 }
 
 // Called every frame
@@ -25,3 +37,24 @@ void AGraspingGame::Tick(float DeltaTime)
 
 }
 
+void AGraspingGame::GetAssetsInFolder(const FString & Directory, TArray<FString> & Assets)
+{
+	Assets.Empty();
+	if (FPaths::DirectoryExists(Directory))
+	{
+		auto Path = Directory + "/*.uasset";
+		UE_LOG(LogTemp, Warning, TEXT("Path: %s"), *Path);
+		FFileManagerGeneric::Get().FindFiles(Assets, *Path, true, false);
+		for (auto & Asset : Assets)
+		{
+			Asset = Directory + Asset;
+		}
+	}
+}
+
+void AGraspingGame::SpawnItem(TArray<FString> & Assets)
+{
+
+	//SpawnedMesh->SetStaticMesh();
+	//SpawnedMesh->
+}
--- Source/RobCoG/GraspingGame.h
+++ Source/RobCoG/GraspingGame.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Engine/TriggerBox.h"
+#include "Components/StaticMeshComponent.h"
 
 #include "GraspingGame.generated.h"
 
@@ -12,26 +13,36 @@ UCLASS()
 class ROBCOG_API AGraspingGame : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
+	// The Paths to be scanned for Items
+	UPROPERTY(EditAnywhere)
+		TArray<FString> Paths;
+
+	// The Box in which the new Items are spawned
 	UPROPERTY(EditAnywhere)
 		ATriggerBox* SpwaningBox;
 
+	// The Box in which the Items have to be placed
 	UPROPERTY(EditAnywhere)
 		ATriggerBox* TargetBox;
 
-
 	// Sets default values for this actor's properties
 	AGraspingGame();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+	UStaticMeshComponent* SpawnedMesh;
+	// All found Items to be found
+	TArray<FString> Items;
 
-	
-	
+	void GetAssetsInFolder(const FString & Directory, TArray<FString> & Assets);
+
+	void SpawnItem(TArray<FString> & Assets);
 };
