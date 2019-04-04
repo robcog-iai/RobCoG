95b82f6081d01d1e491957147e1216d2ab5a7e30
Andrei Haidu <andrei.haidu@yahoo.com>
2019-04-04 11:24:55 +0200

    added copyrights as range

--- Source/RobCoG/RobCoG.Build.cs
+++ Source/RobCoG/RobCoG.Build.cs
@@ -1,4 +1,4 @@
-// Copyright 2019, Institute for Artificial Intelligence - University of Bremen
+// Copyright 2017-2019, Institute for Artificial Intelligence - University of Bremen
 // Author: Andrei Haidu (http://haidu.eu)
 
 using UnrealBuildTool;
