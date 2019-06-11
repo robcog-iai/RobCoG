63ffb66cb01c87d6af6e082e1b21585b9b1609ac
Andrei Haidu <andrei.haidu@yahoo.com>
2019-06-11 13:08:41 +0200

    4.21 branch

---
+++ Source/RobCoG.Target.cs
@@ -0,0 +1,15 @@
+// Copyright 2017-2019, Institute for Artificial Intelligence - University of Bremen
+// Author: Andrei Haidu (http://haidu.eu)
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class RobCoGTarget : TargetRules
+{
+	public RobCoGTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+
+		ExtraModuleNames.AddRange( new string[] { "RobCoG" } );
+	}
+}
